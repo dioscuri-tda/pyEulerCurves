e739a9fb58aa6be9945f802838c8f1a0f81df209
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2025-05-28 19:22:14 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ autoclass_content = "both"
 
 autodoc_mock_imports = [
     "pyEulerCurves._compute_local_EC_VR",  # mock the C++ extension
+    "pyEulerCurves._compute_local_EC_cubical",
     "numpy",
     "pandas",
     "numba",
