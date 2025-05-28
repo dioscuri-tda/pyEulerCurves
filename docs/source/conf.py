aa243a9d40c57d8b791153fb7b8b7ac5b729c246
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2025-05-28 19:07:59 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,8 @@ extensions = [
     # "myst_parser",
 ]
 
+nb_execution_mode = "off"  # disables notebook execution
+
 # Enable MathJax for rendering LaTeX
 # mathjax_path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"
 
