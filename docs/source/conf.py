4456578c6e43255beba70e2e2f53e520bc58354d
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2025-05-29 11:24:30 +0200

    tentative rtd fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../"))
+on_rtd = os.environ.get("READTHEDOCS") == "True"
+
+if not on_rtd:
+    sys.path.insert(0, os.path.abspath("../../"))
 
 print(os.getcwd())
 
